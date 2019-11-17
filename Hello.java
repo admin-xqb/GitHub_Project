964d26552ed1e6c4613d44f2aaf57fa06feec2c3
xqb <850781039@qq.com>
2019-11-17 21:19:07 +0800

    Update Hello.java File,Add Demo.java File

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello(){
           public static void main(String args[]){
 		System.out.println("玻璃小屋！");
 		System.out.println("Hello,Android!");
+		System.out.println("11232");
 	}
 }
