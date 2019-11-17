42fc623b9f2d42c6a196e403df47c6cd307010e2
xqb <850781039@qq.com>
2019-11-17 21:27:43 +0800

    Change Two Files

--- Hello.java
+++ Hello.java
@@ -2,6 +2,5 @@ public class Hello(){
           public static void main(String args[]){
 		System.out.println("玻璃小屋！");
 		System.out.println("Hello,Android!");
-		System.out.println("11232");
 	}
 }
